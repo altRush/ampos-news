50f251cbbe69823950972cb78ced3ad78fc263a9
Rush WP <rushing.wp@gmail.com>
2019-08-20 15:05:09 +0700

    First landing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        className="logo"
+        style={{
+          border: '1px solid #1f1f1f',
+          width: '120px',
+          height: '120px',
+          borderRadius: '50%',
+          margin: '20px 80px 40px 20px',
+          backgroundColor: '#27a69b'
+        }}
+      >
+        Ampos
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
