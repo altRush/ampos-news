a471396b82e8e2d96475e22a943330b36454ca82
Rush WP <rushing.wp@gmail.com>
2019-08-21 05:06:38 +0700

    Logo and Menu bar done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
 import React from 'react'
-import './App.css'
+import { Nav, Container, Row, Col } from 'react-bootstrap'
+
+import './App.scss'
 
 function App() {
+  const menuList = ['News', 'Regions', 'Video', 'TV']
   return (
     <div className="App">
-      <div
-        className="logo"
-        style={{
-          border: '1px solid #1f1f1f',
-          width: '120px',
-          height: '120px',
-          borderRadius: '50%',
-          margin: '20px 80px 40px 20px',
-          backgroundColor: '#27a69b'
-        }}
-      >
-        Ampos
-      </div>
+      <Container>
+        <Row>
+          <Col sm={2}>
+            <div className="logo">Logo</div>
+          </Col>
+          <Col className="menu justify-content-end" sm={10}>
+            <Nav activeKey="/home">
+              {menuList.map((menu, i) => {
+                return (
+                  <Nav.Item>
+                    <div className="nav-menu">
+                      <Nav.Link href={menuList[i]}>{menuList[i]}</Nav.Link>
+                    </div>
+                  </Nav.Item>
+                )
+              })}
+              <div className="search-bar">
+                <input
+                  name="name"
+                  type="search"
+                  className="fa fa-search"
+                  placeholder="&#61442; &nbsp; Search"
+                />
+              </div>
+            </Nav>
+          </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
